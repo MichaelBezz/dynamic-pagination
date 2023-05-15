c906213f23d17123d2505f40a3b0106870ad2685
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-05-15 12:53:44 +0300

    feat: добавит базовую стилизацию Index

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,15 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import IndexPage from '../../pages/index-page/index-page';
+
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<IndexPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/index-page/index-page.tsx
@@ -0,0 +1,20 @@
+import './index-page.css';
+
+
+function IndexPage(): JSX.Element {
+  return (
+    <div className="wrapper">
+      <header className="header" />
+
+      <main>
+        <div className="container">
+
+        </div>
+      </main>
+
+      <footer className="footer" />
+    </div>
+  );
+}
+
+export default IndexPage;
